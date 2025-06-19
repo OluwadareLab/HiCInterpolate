7f19b41491d21dee3165997c291923833aa045b2
hmamohit <hmamohit@gmail.com>
2025-06-19 14:32:53 -0600

    updated paths

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ import torch
 DEVICE = torch.device(
     'cuda' if torch.cuda.is_available() else 'cpu')
 
-ROOT_DIR = f"/home/mohit/Documents/project/interpolation/HiCInterpolate"
-DATA_DIR = f"/home/mohit/Documents/project/interpolation/data/triplets/normalized"
+ROOT_DIR = f"/home/hchowdhu/Documents/HiCInterpolate_Set_1"
+DATA_DIR = f"/mmfs1/home/hchowdhu/data/hic_triplets/normalized"
 
-RECORD_FILE = f"{DATA_DIR}/128/train.txt"
-IMAGE_DIR = f"{DATA_DIR}/128"
+RECORD_FILE = f"{DATA_DIR}/64/train.txt"
+IMAGE_DIR = f"{DATA_DIR}/64"
 INTERPOLATOR_IMAGES_MAP = {
     'frame_0': 'img1.npy',
     'frame_1': 'img2.npy',
@@ -18,6 +18,7 @@ INTERPOLATOR_IMAGES_MAP = {
 TRAIN_VAL_RATIO = [0.85, 0.15]
 BATCH_SIZE = 32
 
+
 NUM_EPOCHS = 1000
 SAVE_EVERY = 10
 LEARNING_RATE = 0.0001
@@ -25,23 +26,24 @@ DECAY_STEPS = 50
 DECAY_RATE = 0.95
 LEARNING_RATE_STAIRCASE = True
 
+
 INIT_IN_CHANNELS = 1
 INIT_OUT_CHANNELS = 4
 
-PYRAMID_LEVEL = 5  # Input pyramid level
-EXT_FEATURE_LEVEL = 4  # Features stack goes to optical flow
+PYRAMID_LEVEL = 4  # Input pyramid level
+EXT_FEATURE_LEVEL = 3  # Features stack goes to optical flow
 
-UNIQUE_LEVELS = 3
-FLOW_NUM_OF_CONVS = [3, 3, 3, 3]
-FLOW_OUT_CHANNELS = [16, 32, 64, 128]
+UNIQUE_LEVELS = 2
+FLOW_NUM_OF_CONVS = [3, 3, 3]
+FLOW_OUT_CHANNELS = [16, 32, 64]
 
-FUSION_PYRAMID_LEVEL = 5
+FUSION_PYRAMID_LEVEL = 4
 
 
 VGG_MODEL_FILE = f"{DATA_DIR}/models/imagenet-vgg-verydeep-19.mat"
 LOSS_WEIGHT_PARAMETERS = [
     {'name': 'charbonnier', 'boundaries': [0], 'values': [1.0, 1.0]},
-    {'name': 'ssim', 'boundaries': [500], 'values': [1.0, 0.8]},
+    {'name': 'ssim', 'boundaries': [0], 'values': [1.0, 1.0]},
     {'name': 'vgg', 'boundaries': [600], 'values': [1.0, 0.40]},
     {'name': 'style', 'boundaries': [400], 'values': [0.0, 0.60]}]
 
@@ -49,7 +51,7 @@ NUM_VISUALIZATION_SAMPLES = 2
 IMG_VAL_PLOT_PATH = os.path.sep.join(
     [ROOT_DIR, "output"])
 
-MODEL_NAME = 'hicinterpolate_128'
+MODEL_NAME = 'hicinterpolate_64'
 MODEL_FILE = os.path.join(ROOT_DIR, "models", f"{MODEL_NAME}.pth")
 TRAIN_VAL_PLOT_FILE = os.path.sep.join(
     [ROOT_DIR, "output", f"{MODEL_NAME}_train_val_plot.png"])
