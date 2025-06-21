71370895d1119ac7e6ea9cdabcd8965379384d6b
hmamohit <hmamohit@gmail.com>
2025-06-20 20:51:01 -0600

    fixed slurm

--- run.sh
+++ run.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 sbatch job1.slurm config_128_set_1
-sbatch job2.slurm.sh config_256_set_1
+sbatch job2.slurm config_256_set_1
 
 sbatch job1.slurm config_128_set_2
-sbatch job2.slurm.sh config_256_set_2
+sbatch job2.slurm config_256_set_2
 
 sbatch job1.slurm config_128_set_3
-sbatch job2.slurm.sh config_256_set_3
+sbatch job2.slurm config_256_set_3
 
 sbatch job1.slurm config_128_set_4
-sbatch job2.slurm.sh config_256_set_4
+sbatch job2.slurm config_256_set_4
 
 sbatch job1.slurm config_128_set_5
-sbatch job2.slurm.sh config_256_set_5
+sbatch job2.slurm config_256_set_5
 
 sbatch job1.slurm config_128_set_6
-sbatch job2.slurm.sh config_256_set_6
+sbatch job2.slurm config_256_set_6
 
