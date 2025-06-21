0d6f878f1bb0559efc51e85cf76319bea6dc8ba3
hmamohit <hmamohit@gmail.com>
2025-06-20 20:42:06 -0600

    fixed slurm

--- run.sh
+++ run.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-torchrun --nproc-per-node 1 --nnodes 1 --node_rank 0 train.py train.py --distributed --config config_64_set_1
-torchrun --nproc-per-node 1 --nnodes 1 --node_rank 0 train.py train.py --distributed --config config_64_set_2
-torchrun --nproc-per-node 1 --nnodes 1 --node_rank 0 train.py train.py --distributed --config config_64_set_3
-torchrun --nproc-per-node 1 --nnodes 1 --node_rank 0 train.py train.py --distributed --config config_64_set_4
-torchrun --nproc-per-node 1 --nnodes 1 --node_rank 0 train.py train.py --distributed --config config_64_set_5
-torchrun --nproc-per-node 1 --nnodes 1 --node_rank 0 train.py train.py --distributed --config config_64_set_6
+sbatch job1.slurm config_128_set_1
+sbatch job2.slurm.sh config_256_set_1
+
+sbatch job1.slurm config_128_set_2
+sbatch job2.slurm.sh config_256_set_2
+
+sbatch job1.slurm config_128_set_3
+sbatch job2.slurm.sh config_256_set_3
+
+sbatch job1.slurm config_128_set_4
+sbatch job2.slurm.sh config_256_set_4
+
+sbatch job1.slurm config_128_set_5
+sbatch job2.slurm.sh config_256_set_5
+
+sbatch job1.slurm config_128_set_6
+sbatch job2.slurm.sh config_256_set_6
+
