d632d274543a1c51ca0bf7b53e04a01c03cdaca8
hmamohit <hmamohit@gmail.com>
2025-07-03 13:03:44 -0600

    added 512 configs

--- run.sh
+++ run.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-sbatch job1.slurm config_128_set_1 &
-sbatch job2.slurm config_256_set_1 &
-sbatch job1.slurm config_128_set_2 &
-sbatch job2.slurm config_256_set_2 &
-wait
-sbatch job1.slurm config_128_set_3 &
-sbatch job2.slurm config_256_set_3 &
-sbatch job1.slurm config_128_set_4 &
-sbatch job2.slurm config_256_set_4 &
-wait
-sbatch job1.slurm config_128_set_5 &
-sbatch job2.slurm config_256_set_5 &
-sbatch job1.slurm config_128_set_6 &
-sbatch job2.slurm config_256_set_6 &
-wait
-
+sbatch job1.slurm config_512_set_1
+sbatch job2.slurm config_512_set_2
+sbatch job1.slurm config_512_set_3
+sbatch job2.slurm config_512_set_4
+sbatch job1.slurm config_512_set_5
+sbatch job2.slurm config_512_set_6
