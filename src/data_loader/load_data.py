2166d500dd44c7c5259be8518a326aabd341253e
hc0783 <UNT\hc0783@ceng-l-jzr95g4.unt.ad.unt.edu>
2026-05-27 05:31:49 +0000

    updated dl

--- src/data_loader/load_data.py
+++ src/data_loader/load_data.py
@@ -23,6 +23,8 @@ class TripletDataset(Dataset):
 
     def get_image(self, image_file: str) -> Tensor:
         img = np.load(image_file)
+        if np.isnan(img).any() or np.isinf(img).any() or img.max() == 0:
+            return None
         # img = self.log1p(img)
         # img = self.min_max_norm(img)
         img = torch.from_numpy(img).float().unsqueeze(0)
